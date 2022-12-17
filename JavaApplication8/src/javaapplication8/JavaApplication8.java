2cff8130a3bda0c31d961a202880379a5d5acbc9
AbnerRodrigues10 <54107473+AbnerRodrigues10@users.noreply.github.com>
2022-12-17 17:13:31 -0300

    Swing idioma
    integrar a função idioma na interface swing

---
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication8;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author AbnerRodrigues
+ */
+public class JavaApplication8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         // declaring object of Locale
+        Locale locale;
+        
+        // calling the getDefault method
+        locale = Locale.getDefault();
+  
+        // printing the locale
+        System.out.println(locale);
+    }
+    
+}
