bf19684f41d95087628b0bf4322fd636b6941a72
AbnerRodrigues10 <54107473+AbnerRodrigues10@users.noreply.github.com>
2023-01-23 15:54:07 -0300

    Aula 4.H
    Condicional if else
    operadores lógicos e relacionais

---
+++ StartByCapgemini/CondicionaisSe/app/src/main/java/CondicionaisSe/App.java
@@ -0,0 +1,32 @@
+package CondicionaisSe;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        int media = 7;
+        
+        // > maior
+        // >= maior ou igual
+        // < menor 
+        // <= igual ou menor
+        // == é totalmente igual
+        // != diferente 
+        
+        // operador logico 
+        // && E
+        // || ou 
+        // ! negação
+        
+        
+        if (media >= 7) {
+            if(media == 10) {
+            // aprovado com nota maxima
+            } else {
+                // aprovado
+            }
+        } else {
+            // reprovado
+        }
+    }
+}
